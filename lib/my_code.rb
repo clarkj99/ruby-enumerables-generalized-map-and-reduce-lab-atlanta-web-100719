d77f19d9d834461ad5e3fdb7b4cc575bfbd6eb79
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-09-10 00:24:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # Your Code Here
 def map (source_array)
   new_array =[]
-  source_array.times do |index| 
+  source_array.length.times do |index| 
      new_array.push (yield (source_array[index]))
   end
   new_array
