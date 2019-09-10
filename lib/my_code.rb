450e2d776a3daf0680510afd50724cd277949b61
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-09-10 00:25:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,11 @@ def map (source_array)
   end
   new_array
 end
+
+def reduce (source_array)
+  new_array =[]
+  source_array.length.times do |index| 
+     new_array.push (yield (source_array[index]))
+  end
+  new_array
+end
