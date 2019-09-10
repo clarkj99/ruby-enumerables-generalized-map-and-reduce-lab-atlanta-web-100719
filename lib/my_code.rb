fe77e46948f3e6ad5fd0903813c6fd0db40491ce
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-09-10 00:28:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,9 +8,9 @@ def map (source_array)
 end
 
 def reduce (source_array, starting_point=0)
-  new_array =[]
+  memo = starting_point
   source_array.length.times do |index| 
-     new_array.push (yield (source_array[index]))
+     memo =  (yield (source_array[index]))
   end
   new_array
 end
